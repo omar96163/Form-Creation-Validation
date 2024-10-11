8ef750ea62cb380199e509cad2044d49fddc44e8
Omar Albaz <omaralbaz635@gmail.com>
2024-10-11 18:58:31 +0300

    creat : index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const form = document.getElementById('registration-form')
+    const feedbackDiv = document.getElementById('form-feedback')
+    form.addEventListener('submit',(event) => {
+        event.preventDefault()
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+        let isValid = true;
+        const messages = []
+        if (username.length < 3 ) {
+            isValid = false;
+            messages.push ('error username')
+        }
+        if ( !email.includes('@')  ||  !email.includes('.')) {
+            isValid = false;
+            messages.push ('error email')
+        }
+        if (password.length < 8) {
+            isValid = false;
+            messages.push ('error password')
+        }
+        feedbackDiv.style.display = "block"
+        if (isValid) {
+            feedbackDiv.textContent = "Registration successful!"
+            feedbackDiv.style.color = "#28a745"
+        }
+        else {
+            feedbackDiv.innerHTML = messages.join('<br>')
+            feedbackDiv.style.color = "#dc3545"
+        }
+
+    })
+})
